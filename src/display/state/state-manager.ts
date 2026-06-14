cb843cec6a3c21e9da83583323cea5621ff596de
gallfeder <finnick4gallfeder@gmail.com>
2026-06-14 19:21:39 +0200

    Added caching functionality to state

--- src/display/state/state-manager.ts
+++ src/display/state/state-manager.ts
@@ -1,15 +1,18 @@
 import {CallbackFunction} from "../../shared/state-manager";
+import {Messdiener} from "../../shared/general";
 
 interface SubscriptionListElement<T> {
     fn: CallbackFunction<T>
     id: number
 }
 
-class SubscriptionList<T> {
-    private list: SubscriptionListElement<T>[];
+class State<T> {
+    private subscriptions: SubscriptionListElement<T>[];
     private idCounter: number;
+    private savedData: T;
+
     constructor() {
-        this.list = [];
+        this.subscriptions = [];
         this.idCounter = 0;
     }
     
@@ -18,20 +21,27 @@ class SubscriptionList<T> {
             fn: fn,
             id: this.idCounter++,
         }
-        this.list.push(elem)
+        this.subscriptions.push(elem)
+        fn(this.savedData)
+            .catch(() => this.remove(elem.id))
         return () => {
             this.remove(elem.id)
         }
     }
     pushUpdate(data: T) {
-        for (const elem of this.list) {
-            elem.fn(data)
+        this.savedData = data
+        for (const elem of this.subscriptions) {
+            elem.fn(this.savedData)
                 .catch(() => this.remove(elem.id))
         }
     }
     private remove(id: number) {
-        this.list = this.list.filter(e => e.id !== id)
+        this.subscriptions = this.subscriptions.filter(e => e.id !== id)
     }
+}
+
+const states = {
+    AllMessdiener: new State<Array<Messdiener>>()
 }
 
 
