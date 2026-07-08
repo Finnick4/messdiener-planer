9000d4c3fc6d12903b232eb4bea4b0a45a01a02b
gallfeder <finnick4gallfeder@gmail.com>
2026-07-08 23:44:50 +0200

    Added allocating Messdiener to mass

--- src/display/components/family/family-adder.ts
+++ src/display/components/family/family-adder.ts
@@ -21,26 +21,49 @@ export class FamilyAdder extends HTMLElement {
         this.onedit(ids);
         this.closeSubscription = addSubscription(ListenerEndpoints.AllFamilies, (data: Family[]) => {
             const selectableFamilies = data.filter(family => !this.selectedFamilies.has(family.id));
+            let familyPoolSize = selectableFamilies.length;
+
+            const checkIfEmpty = () => {
+                if (familyPoolSize == 0) {
+                    const placeholder = document.createElement("p");
+                    placeholder.classList.add("placeholder");
+                    placeholder.innerHTML = "Es bestehen keine Familien, welche der Messe zugewiesen werden können!"
+                    this.replaceChildren(placeholder);
+                }
+            }
 
             const makeElement = (family: Family): HTMLDivElement => {
                 const elem = document.createElement("div");
+                const sizeTag = document.createElement("div");
                 const nameElem = document.createElement("div");
                 const countElem = document.createElement("div");
                 const addBtn = document.createElement("button");
+
+                sizeTag.innerText = String(family.memberSize);
                 nameElem.innerText = createInternalFamilyName(family.lastNameInternal, family.lastNameDisplay);
                 countElem.innerText = "0 Messen";
                 addBtn.innerText = "+";
 
+                sizeTag.classList.add("tag");
                 countElem.classList.add("value");
+
+                addBtn.addEventListener("click", () => {
+                    this.selectedFamilies.add(family.id);
+                    elem.remove();
+                    familyPoolSize--;
+                    checkIfEmpty();
+                    this.onedit(this.selectedFamilies);
+                })
 
                 elem.classList.add("row", "entry");
                 elem.dataset.familyId = String(family.id);
-                elem.replaceChildren(nameElem, countElem, addBtn);
+                elem.replaceChildren(sizeTag, nameElem, countElem, addBtn);
 
                 return elem;
             }
 
             this.replaceChildren(...(selectableFamilies.map(family => makeElement(family))));
+            checkIfEmpty();
         })
     }
 
--- src/display/components/messdiener/allocator.ts
+++ src/display/components/messdiener/allocator.ts
@@ -1,5 +1,7 @@
 import {MessdienerPreparedList} from "./prepared-list";
 import {FamilyAdder} from "../family/family-adder";
+import {addSubscription, ListenerEndpoints} from "../../state/state-manager";
+import {Messdiener} from "../../../shared/general";
 
 export class MessdienerAllocator extends HTMLElement {
     constructor() {
@@ -14,16 +16,46 @@ export class MessdienerAllocator extends HTMLElement {
             this.allocatedIDs.delete(id);
             this.onedit(this.allocatedIDs);
         }
-        this.appendChild(messdienerList)
+        this.appendChild(messdienerList);
 
         const familyAdder = document.createElement("family-adder") as FamilyAdder;
 
-        this.appendChild(familyAdder)
+        this.appendChild(familyAdder);
 
         this.setAllocatedMessdiener = (ids: Set<number>) => {
             this.allocatedIDs = ids;
             messdienerList.changePickedMessdiener(this.allocatedIDs);
+
+            const cancel = addSubscription(ListenerEndpoints.AllMessdiener, (data: Messdiener[]) => {
+                const mapped = new Map<number, Messdiener>(data.map((m) => [m.identifier, m]));
+                const allocatedFamilies = new Set<number>();
+
+                this.allocatedIDs.forEach(messdienerID => {
+                    const messdiener = mapped.get(messdienerID);
+                    if (messdiener) {
+                        allocatedFamilies.add(messdiener.familyID);
+                    }
+                })
+                familyAdder.setSelectedFamilies(new Set<number>(allocatedFamilies));
+
+                familyAdder.onedit = (selectedFamilies: Set<number>)=>  {
+                    let addedFamilyID = 0;
+                    selectedFamilies.forEach(family => {
+                        if (!allocatedFamilies.has(family)) {
+                            addedFamilyID = family;
+                        }
+                    })
+                    data.filter(messdiener => messdiener.familyID == addedFamilyID).forEach(messdiener => {
+                        this.allocatedIDs.add(messdiener.identifier);
+                        this.onedit(this.allocatedIDs);
+                        messdienerList.changePickedMessdiener(this.allocatedIDs);
+                    })
+                }
+
+                cancel();
+            })
         };
+
     }
 
     getAllocatedMessdiener(): Set<number> {
