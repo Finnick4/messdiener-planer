77b392fa75f2f1b1a7b03c9de2d941047f00ee79
gallfeder <finnick4gallfeder@gmail.com>
2026-08-19 17:21:23 +0200

    Fixed electron forge config for hopefully the last time

--- forge.config.ts
+++ forge.config.ts
@@ -9,16 +9,20 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 
 const config: ForgeConfig = {
   packagerConfig: {
-    asar: true,
-    icon: "./assets/icon"
+    asar: {
+      unpack: "*.{node,dll}",
+    },
+    ignore: [/node_modules\/(?!(sqlite3|bindings|file-uri-to-path)\/)/],
+    icon: "./assets/icon",
+    executableName: "messdiener-planer"
   },
   rebuildConfig: {},
   makers: [
     new MakerSquirrel({}),
     new MakerZIP({}, [
-        "darwin",
-        "linux",
-        "win32"
+      "darwin",
+      "linux",
+      "win32"
     ]),
     new MakerRpm({
       options: {icon: "./assets/icon.png"}
