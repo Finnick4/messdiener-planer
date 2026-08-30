08e25d335a51cc0718f01958135b5507058b4b0c
gallfeder <finnick4gallfeder@gmail.com>
2026-08-30 18:07:31 +0200

    Updated README.md with pictures and new features.

--- forge.config.ts
+++ forge.config.ts
@@ -12,7 +12,7 @@ const config: ForgeConfig = {
     asar: {
       unpack: "*.{node,dll}",
     },
-    ignore: [/node_modules\/(?!(sqlite3|node-tectonic|bindings|file-uri-to-path)\/)/],
+    ignore: [/node_modules\/(?!(sqlite3|node-tectonic|bindings|file-uri-to-path)\/)/, "example/"],
     icon: "./assets/icon",
     executableName: "messdiener-planer"
   },
