c6159f9119acc89e4c2cdcd28133d57288c4d8ec
gallfeder <finnick4gallfeder@gmail.com>
2026-09-08 17:00:30 +0200

    Added .ico and build workflow

--- forge.config.ts
+++ forge.config.ts
@@ -6,6 +6,7 @@ import { MakerRpm } from '@electron-forge/maker-rpm';
 import { VitePlugin } from '@electron-forge/plugin-vite';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
+import PublisherGithub from "@electron-forge/publisher-github";
 
 const config: ForgeConfig = {
   packagerConfig: {
@@ -62,6 +63,15 @@ const config: ForgeConfig = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+      new PublisherGithub({
+        repository: {
+          owner: "Finnick4",
+          name: "messdiener-planer"
+        },
+        draft: true,
+      })
+  ]
 };
 
 export default config;
